8b68f9fdf4994d09c24d76729840483e74a333ce
Raka Schipperheijn <raka@rpkschipperheijn.com>
2024-06-13 17:48:45 +0200

    created ray base class

---
+++ src/ray.rs
@@ -0,0 +1,24 @@
+use super::vec::{Point3, Vec3};
+
+pub struct Ray {
+    origin: Point3,
+    direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Ray {
+        Ray { origin, direction }
+    }
+
+    pub fn origin(&self) -> Point3 {
+        self.origin
+    }
+
+    pub fn direction(&self) -> Point3 {
+        self.direction
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        self.origin + t * self.direction
+    }
+}
