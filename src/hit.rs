2d9ea5fff710f01a2ee6cecc394d0295dfbddb9f
Raka Schipperheijn <raka@rpkschipperheijn.com>
2024-06-13 20:42:41 +0200

    created spheres and a world

---
+++ src/hit.rs
@@ -0,0 +1,40 @@
+use super::ray::Ray;
+use super::vec::{Point3, Vec3};
+
+pub struct HitRecord {
+    pub point: Point3,
+    pub normal: Vec3,
+    pub time: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, ray: &Ray, outward_normal: Vec3) {
+        self.front_face = ray.direction().dot(outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            outward_normal
+        } else {
+            (-1.0) * outward_normal
+        };
+    }
+}
+
+pub trait Hit {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
+}
+
+pub type World = Vec<Box<dyn Hit>>;
+
+impl Hit for World {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let mut tmp_record = None;
+        let mut closest_so_far = t_max;
+        for object in self {
+            if let Some(record) = object.hit(r, t_min, closest_so_far) {
+                closest_so_far = record.time;
+                tmp_record = Some(record);
+            }
+        }
+        tmp_record
+    }
+}
---
+++ src/sphere.rs
@@ -0,0 +1,48 @@
+use super::hit::{Hit, HitRecord};
+use super::ray::Ray;
+use super::vec::{Point3, Vec3};
+
+pub struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    pub fn new(center: Point3, radius: f64) -> Sphere {
+        Sphere { center, radius }
+    }
+}
+
+impl Hit for Sphere {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let oc = ray.origin() - self.center;
+        let a = ray.direction().length().powi(2);
+        let b = oc.dot(ray.direction());
+        let c = oc.length().powi(2) - self.radius.powi(2);
+
+        let discriminant = b.powi(2) - a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let sqrtd = discriminant.sqrt();
+        let mut root = (-b - sqrtd) / a;
+        if root < t_min || t_max < root {
+            root = (-b + sqrtd) / a;
+            if root < t_min || t_max < root {
+                return None;
+            }
+        }
+
+        let mut record = HitRecord {
+            time: root,
+            point: ray.at(root),
+            normal: (ray.at(root) - self.center) / self.radius,
+            front_face: false,
+        };
+
+        let outward_normal = (record.point - self.center) / self.radius;
+        record.set_face_normal(ray, outward_normal);
+        Some(record)
+    }
+}
